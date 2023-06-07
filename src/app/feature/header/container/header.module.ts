57384ae2a5e298b6502be2ccfb08bf9ce86a1702
Beatrice Paoli <beatrice.paoli@magentalab.it>
2023-06-07 14:17:10 +0200

    added header + app logos

--- src/app/feature/header/container/header.module.ts
+++ src/app/feature/header/container/header.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 
@@ -9,7 +15,13 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
   ],
   exports: [
     HeaderComponent
