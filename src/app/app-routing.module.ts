9bacc1bfe121f7427ddf14432d69d23af1eab285
Beatrice Paoli <beatrice.paoli@magentalab.it>
2023-06-08 23:16:13 +0200

    added routing to movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// TODO: evitare ripetizioni
 const routes: Routes = [
   {
     path: '',
@@ -15,10 +16,24 @@ const routes: Routes = [
       ),
   },
   {
+    path: 'movie-list-now/:movieId',
+    loadChildren: () =>
+      import('./feature/movie/movie.module').then(
+        (m) => m.MovieModule
+      ),
+  },
+  {
     path: 'movie-list-next',
     loadChildren: () =>
       import('./feature/movie-list/movie-list.module').then(
         (m) => m.MovieListModule
+      ),
+  },
+  {
+    path: 'movie-list-next/:movieId',
+    loadChildren: () =>
+      import('./feature/movie/movie.module').then(
+        (m) => m.MovieModule
       ),
   },
   {
--- src/app/feature/movie/movie.module.ts
+++ src/app/feature/movie/movie.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieComponent } from './container/movie.component';
-import { StoreModule } from '@ngrx/store';
-import * as fromMovie from './store/reducers/movie.reducer';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { MovieComponent } from './container/movie.component';
 import { MovieEffects } from './store/effects/movie.effects';
+import * as fromMovie from './store/reducers/movie.reducer';
 
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -14,6 +22,7 @@ import { MovieEffects } from './store/effects/movie.effects';
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     StoreModule.forFeature(fromMovie.movieFeatureKey, fromMovie.reducer),
     EffectsModule.forFeature([MovieEffects])
   ]
