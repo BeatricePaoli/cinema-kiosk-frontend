dd4e10b9cb97ac73eb82a96f691eff359e473f17
Beatrice Paoli <beatrice.paoli@magentalab.it>
2023-08-12 12:08:58 +0200

    fix displayed values in booking summary

--- src/app/shared/components/booking-summary/booking-summary.component.ts
+++ src/app/shared/components/booking-summary/booking-summary.component.ts
@@ -1,7 +1,5 @@
-import { Component, Input } from '@angular/core';
-import * as _moment from 'moment';
-
-const moment = _moment;
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ImgSanitizerService } from 'src/app/core/services/img-sanitizer.service';
 
 
 @Component({
@@ -9,12 +7,28 @@ const moment = _moment;
   templateUrl: './booking-summary.component.html',
   styleUrls: ['./booking-summary.component.scss']
 })
-export class BookingSummaryComponent {
+export class BookingSummaryComponent implements OnChanges {
 
   @Input()
   booking: any;
 
   @Input()
   toComplete: boolean = false;
+
+  constructor(private imgSanitizer: ImgSanitizerService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['booking'] && changes['booking'].currentValue) {
+      const b = changes['booking'].currentValue;
+      this.booking = {
+        ...b,
+        movie: {
+          id: b.movie.id,
+          name: b.movie.name,
+          img: this.imgSanitizer.sanitizeImg(b.movie.img.toString()),
+        }
+      }
+    }
+  }
 
 }
